8691ecfaeb5fb13875c1090b7661610b7052303c
saranshparihar29-lab <saranshparihar29@gmail.com>
2026-09-23 05:41:04 +0530

    Time: 0 ms (100%), Space: 15.2 MB (15.96%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -7,16 +7,7 @@ public:
         int temp;
         int n= nums.size();
         vector<int> ans;
-        for(i=0;i<n;i++){
-            for(j=i+1;j<n;j++){
-                if(nums[i]>nums[j]){
-                    temp=nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
-
-                }
-            }
-        }
+        sort(nums.begin(),nums.end());
         for(k=0;k<n;k++){
             if(nums[k]==target){
                 ans.push_back(k);
